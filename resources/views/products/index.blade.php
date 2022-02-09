784eff7ccee09093aba6b70bc65c744bf380deee
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-09 19:16:00 +0100

    Aggiunto stile

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,22 +1,29 @@
 @extends('layout.base')
 
 @section('pageContent')
-    <ul>
-        @foreach ($comics as $aComic)
-            <li>{{$aComic->title}}</li>
-        @endforeach
-    </ul>
-
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
-          <div class="col">col</div>
-          <div class="col">col</div>
-          <div class="col">col</div>
-          <div class="col">col</div>
-        </div>
-        <div class="row">
-          <div class="col-8">col-8</div>
-          <div class="col-4">col-4</div>
+            <div class="col-8 px-5">
+                <div class="row">
+                    @foreach ($comics as $aComic)
+                        <div class="col-3">
+                            <div class="card mb-5">
+                                <img src="{{$aComic->url_img}}" class="card-img-top" alt="{{$aComic->title}}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{$aComic->title}}</h5>
+                                    <h5 class="card-title">Price: ${{$aComic->price}}</h5>
+                                    <p class="card-text">{{$aComic->description}}</p>
+                                    <p class="card-text">{{$aComic->series}}</p>
+                                    <p class="card-text">{{$aComic->sale_date}}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>    
+            </div>
+            <div class="col-4 px-5">
+                <h2>Inserisci un fumetto</h2>
+            </div>
         </div>
     </div>
 @endsection
