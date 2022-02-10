1728711e4a7f3a5c2d440afdf88669120e3601eb
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-10 17:28:41 +0100

    Delete

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,12 +14,18 @@
                             <p class="card-text">{{$aComic->sale_date}}</p>
                         </div>
 
-                        <div class="container-fluid d-flex justify-content-around flex-wrap">
+                        <div class="container-fluid d-flex justify-content-around flex-wrap pb-3">
+                            {{-- <a class="mb-3" href="{{route("comics.show", $aComic->id)}}"></a> --}}
                             <a class="mb-3" href="http://127.0.0.1:8000/comics/{{$aComic->id}}"><button type="button" class="btn btn-primary">PIU' INFO</button></a>
-    
+                            {{-- <a class="mb-3" href="{{route("comics.edit", $aComic->id)}}"></a> --}}
                             <a class="mb-3" href="http://127.0.0.1:8000/comics/{{$aComic->id}}/edit"><button type="button" class="btn btn-warning">MODIFICA</button></a>
 
-                            <a class="my-3" href="http://127.0.0.1:8000/comics/{{$aComic->id}}/edit"><button type="button" class="btn btn-danger">ELIMINA</button></a>
+                            <form action="{{route("comics.destroy", $aComic->id)}}" method="POST">
+                                @csrf
+                                @method("DELETE")
+                                <button type="button" class="btn btn-danger">ELIMINA</button>
+                                {{-- <a class="my-3" href="http://127.0.0.1:8000/comics/{{$aComic->id}}/edit"></a> --}}
+                            </form>
                         </div>
                     </div>
                 </div>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -8,19 +8,24 @@
                 <img src="{{$comic->url_img}}" class="card-img-top" alt="{{$comic->title}}">
                 </div>
                 <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">{{$comic->title}}</h5>
-                    <h5 class="card-title">Price: ${{$comic->price}}</h5>
-                    <p class="card-text">{{$comic->series}}</p>
-                    <p class="card-text">{{$comic->sale_date}}</p>
-                    <p class="card-text">{{$comic->description}}</p>
+                    <div class="card-body">
+                        <h5 class="card-title">{{$comic->title}}</h5>
+                        <h5 class="card-title">Price: ${{$comic->price}}</h5>
+                        <p class="card-text">{{$comic->series}}</p>
+                        <p class="card-text">{{$comic->sale_date}}</p>
+                        <p class="card-text">{{$comic->description}}</p>
 
-                    <a href="http://127.0.0.1:8000/comics"><button type="button" class="btn btn-primary">TORNA ALLA LISTA</button></a>
+                        <a href="http://127.0.0.1:8000/comics"><button type="button" class="btn btn-primary">TORNA ALLA LISTA</button></a>
 
-                    <a class="ml-3" href="http://127.0.0.1:8000/comics/{{$comic->id}}/edit"><button type="button" class="btn btn-warning">MODIFICA</button></a>
+                        <a class="ml-3" href="http://127.0.0.1:8000/comics/{{$comic->id}}/edit"><button type="button" class="btn btn-warning">MODIFICA</button></a>
 
-                    <a class="ml-5" href="http://127.0.0.1:8000/comics/{{$comic->id}}/edit"><button type="button" class="btn btn-danger">ELIMINA</button></a>
-                </div>
+                        <form action="{{route("comics.destroy", $comic->id)}}" method="POST">
+                            @csrf
+                            @method("DELETE")
+                            <button type="button" class="mt-5 btn btn-danger">ELIMINA</button>
+                            {{-- <a class="my-3" href="http://127.0.0.1:8000/comics/{{$aComic->id}}/edit"></a> --}}
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
