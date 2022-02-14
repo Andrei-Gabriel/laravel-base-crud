5726c764171a88ab7359ce95d480439baa4c1f27
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-14 17:29:31 +0100

    + metodo old per salvare i dati inseriti prev

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,34 +7,34 @@
             @csrf
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title" placeholder="Inserisci un titolo">
+                <input type="text" value="{{old("title")}}" class="form-control" id="title" name="title" placeholder="Inserisci un titolo">
             </div>
             <div class="form-group">
                 <label for="url_img">URL immagine</label>
-                <input type="text" class="form-control" id="url_img" name="url_img" placeholder="Inserisci un'immagine">
+                <input type="text" class="form-control" value="{{old("url_img")}}" id="url_img" name="url_img" placeholder="Inserisci un'immagine">
             </div>
             <div class="form-group">
                 <label for="price">Prezzo</label>
-                <input type="number" class="form-control" id="price" name="price" step=".01" placeholder="Inserisci il prezzo">
+                <input type="number" class="form-control" value="{{old("price")}}" id="price" name="price" step=".01" placeholder="Inserisci il prezzo">
             </div>
             <div class="form-group">
                 <label for="series">Serie</label>
-                <input type="text" class="form-control" id="series" name="series" placeholder="Inserisci la serie">
+                <input type="text" class="form-control" value="{{old("series")}}" id="series" name="series" placeholder="Inserisci la serie">
             </div>
             <div class="form-group">
                 <label for="sale_date">Data di vendita</label>
-                <input type="date" min="1970-01-01" max="2025-12-31" class="form-control" id="sale_date" name="sale_date">
+                <input type="date" min="1970-01-01" max="2025-12-31" value="{{old("sale_date")}}" class="form-control" id="sale_date" name="sale_date">
             </div>
             <div class="form-group">
                 <label for="type">Tipo fumetto</label>
                 <select class="form-control" id="type" name="type">
-                    <option value="comic book">Comic book</option>
-                    <option value="graphic novel">Graphic novel</option>
+                    <option value="comic book" {{old("type") == "comic book" ? "selected" : null}}>Comic book</option>
+                    <option value="graphic novel" {{old("type") == "graphic novel" ? "selected" : null}}>Graphic novel</option>
                 </select>
             </div>
             <div class="form-group">
                 <label for="description">Descrizione del fumetto</label>
-                <textarea class="form-control" id="description" name="description" rows="7"></textarea>
+                <textarea class="form-control" id="description" name="description" rows="7">{{old("description")}}</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Aggiungi il fumetto</button>
         </form>
