f4b39cfe5ba902b3a170fd6c033f8c4b40256137
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-14 17:39:00 +0100

    + messaggio di errore sotto il campo non valido

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,46 +7,58 @@
             @csrf
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" value="{{old("title")}}" class="form-control" id="title" name="title" placeholder="Inserisci un titolo">
+                {{-- is-invalid è una classe di Bootstrap --}}
+                <input type="text" value="{{old("title")}}" class="form-control @error('title') is-invalid @enderror" id="title" name="title" placeholder="Inserisci un titolo">
+                @error('title')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="url_img">URL immagine</label>
-                <input type="text" class="form-control" value="{{old("url_img")}}" id="url_img" name="url_img" placeholder="Inserisci un'immagine">
+                <input type="text" class="form-control @error('url_img') is-invalid @enderror" value="{{old("url_img")}}" id="url_img" name="url_img" placeholder="Inserisci un'immagine">
+                @error('url_img')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="price">Prezzo</label>
-                <input type="number" class="form-control" value="{{old("price")}}" id="price" name="price" step=".01" placeholder="Inserisci il prezzo">
+                <input type="number" class="form-control @error('price') is-invalid @enderror" value="{{old("price")}}" id="price" name="price" step=".01" placeholder="Inserisci il prezzo">
+                @error('price')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="series">Serie</label>
-                <input type="text" class="form-control" value="{{old("series")}}" id="series" name="series" placeholder="Inserisci la serie">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" value="{{old("series")}}" id="series" name="series" placeholder="Inserisci la serie">
+                @error('series')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="sale_date">Data di vendita</label>
-                <input type="date" min="1970-01-01" max="2025-12-31" value="{{old("sale_date")}}" class="form-control" id="sale_date" name="sale_date">
+                <input type="date" min="1970-01-01" max="2025-12-31" value="{{old("sale_date")}}" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date">
+                @error('sale_date')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="type">Tipo fumetto</label>
-                <select class="form-control" id="type" name="type">
+                <select class="form-control @error('type') is-invalid @enderror" id="type" name="type">
                     <option value="comic book" {{old("type") == "comic book" ? "selected" : null}}>Comic book</option>
                     <option value="graphic novel" {{old("type") == "graphic novel" ? "selected" : null}}>Graphic novel</option>
                 </select>
+                @error('type')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="description">Descrizione del fumetto</label>
-                <textarea class="form-control" id="description" name="description" rows="7">{{old("description")}}</textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="7">{{old("description")}}</textarea>
+                @error('description')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Aggiungi il fumetto</button>
         </form>
-
-        @if ($errors->any())
-            <div class="alert alert-danger mt-3">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
     </div>
 @endsection
