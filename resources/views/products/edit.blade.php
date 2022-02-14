3fdcd9def4d6f299afa8e465cab307470ca9fd6d
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-02-14 18:17:47 +0100

    + metodo old nell'edit

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -8,35 +8,56 @@
             @method("PUT")
             <div class="form-group">
                 <label for="title">Titolo</label>
-                <input type="text" class="form-control" id="title" name="title" value="{{$comic->title}}" placeholder="Inserisci un titolo">
+                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old("title") ? old("title") : $comic->title}}" placeholder="Inserisci un titolo">
+                @error('title')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="url_img">URL immagine</label>
-                <input type="text" class="form-control" id="url_img" name="url_img" value="{{$comic->url_img}}" placeholder="Inserisci un'immagine">
+                <input type="text" class="form-control @error('url_img') is-invalid @enderror" id="url_img" name="url_img" value="{{old("url_img") ? old("url_img") : $comic->url_img}}" placeholder="Inserisci un'immagine">
+                @error('url_img')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="price">Prezzo</label>
-                <input type="number" class="form-control" id="price" value="{{$comic->price}}"" name="price" step=".01" placeholder="Inserisci il prezzo">
+                <input type="number" class="form-control @error('price') is-invalid @enderror" id="price" value="{{old("price") ? old("price") : $comic->price}}" name="price" step=".01" placeholder="Inserisci il prezzo">
+                @error('price')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="series">Serie</label>
-                <input type="text" class="form-control" id="series" name="series" value="{{$comic->series}}" placeholder="Inserisci la serie">
+                <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old("series") ? old("seires") : $comic->series}}" placeholder="Inserisci la serie">
+                @error('series')
+                <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="sale_date">Data di vendita</label>
-                <input type="date" min="1970-01-01" max="2025-12-31" class="form-control" value="{{$comic->sale_date}}" id="sale_date" name="sale_date">
+                <input type="date" min="1970-01-01" max="2025-12-31" class="form-control @error('sale_date') is-invalid @enderror" value="{{$comic->sale_date}}" id="sale_date" name="sale_date">
+                @error('sale_date')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="type">Tipo fumetto</label>
-                <select class="form-control" id="type" name="type">
+                <select class="form-control @error('type') is-invalid @enderror" id="type" name="type">
                     <option value="comic book" {{$comic->type == "comic book" ? "selected" : ""}}>Comic book</option>
                     <option value="graphic novel" {{$comic->type == "graphic novel" ? "selected" : ""}}>Graphic novel</option>
                 </select>
+                @error('type')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="description">Descrizione del fumetto</label>
                 {{-- textarea non ha value --}}
-                <textarea class="form-control" id="description" name="description" rows="7">{{$comic->description}}</textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description" rows="7">{{old("description") ? old("description") : $comic->description}}</textarea>
+                @error('description')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
             <button type="submit" class="btn btn-primary">Modifica</button>
         </form>
